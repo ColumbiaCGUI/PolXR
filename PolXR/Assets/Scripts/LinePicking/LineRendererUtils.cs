d9796c2ec067f38f7ca2ac6b76098359c863cb8a
David Rios <dar2209@columbia.edu>
2025-05-02 17:46:17 -0400

    feat: unguided line picking mode added

--- PolXR/Assets/Scripts/LinePicking/LineRendererUtils.cs
+++ PolXR/Assets/Scripts/LinePicking/LineRendererUtils.cs
@@ -1,7 +1,19 @@
-﻿namespace LinePicking
+﻿using UnityEngine;
+
+namespace LinePicking
 {
-    public class LineRendererUtils
+    public static class LineRendererUtils
     {
-        
+        public static void InitializeLineRenderer(LineRenderer lineRenderer, Color lineColor)
+        {
+            lineRenderer.startWidth = 0.02f;
+            lineRenderer.endWidth = 0.02f;
+            
+            // Set the color of the line using the Unlit/Color shader
+            lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+
+            lineRenderer.startColor = lineColor;
+            lineRenderer.endColor = lineColor; 
+        }
     }
 }
