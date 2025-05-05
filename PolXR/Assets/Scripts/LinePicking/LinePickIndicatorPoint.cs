6a7e7c537699b6d83e7f7ba47fa48bbb120db7f4
David Rios <dar2209@columbia.edu>
2025-05-05 00:29:40 -0400

    feat: added ray intersection indicator

---
+++ PolXR/Assets/Scripts/LinePicking/LinePickIndicatorPoint.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace LinePicking
+{
+    public class LinePickIndicatorPoint : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] indicatorMeshes;
+
+        [SerializeField] private GameObject offsetParent;
+
+        [SerializeField] private XRRayInteractor rightControllerRayInteractor;
+
+        [SerializeField] private ToggleLinePickingMode linePickingState;
+
+        private void HideIndicator()
+        {
+            foreach (GameObject indicatorMeshObj in indicatorMeshes)
+            {
+                indicatorMeshObj.gameObject.SetActive(false);
+            }
+        }
+        
+        private void ShowIndicator()
+        {
+            foreach (GameObject indicatorMeshObj in indicatorMeshes)
+            {
+                indicatorMeshObj.gameObject.SetActive(true);
+            }
+        }
+        
+        private void Update()
+        {
+            if (linePickingState.isLinePickingEnabled && rightControllerRayInteractor.TryGetCurrent3DRaycastHit(out var raycastHit))
+            {
+                if (!raycastHit.transform.name.Contains("Data"))
+                {
+                    HideIndicator();
+                    return;
+                }
+
+                offsetParent.transform.position = raycastHit.point;
+                offsetParent.transform.rotation = Quaternion.LookRotation(raycastHit.normal);
+                ShowIndicator();
+                return;
+            }
+            
+            HideIndicator();
+        }
+    }
+}
