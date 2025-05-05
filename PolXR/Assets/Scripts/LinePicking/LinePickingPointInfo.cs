0dd37b7d450e81b0f77deaf3c8957102c4ca736b
David Rios <dar2209@columbia.edu>
2025-05-04 23:44:25 -0400

    chore: cleaned up line picking code a bit

---
+++ PolXR/Assets/Scripts/LinePicking/LinePickingPointInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace LinePicking
+{
+    public class LinePickingPointInfo
+    {
+        public Transform HitRadargram;
+
+        public Vector3 Point;
+
+        public Vector2 UVCoordinates;
+
+        public Vector3 HitNormal;
+
+        public GameObject DebugVisual;
+
+        public GameObject LineVisual;
+    }
+}
